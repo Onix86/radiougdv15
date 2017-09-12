18e6698eecc568c70c6528f9b5d0d070cb58d612
Onix86 <31633892+Onix86@users.noreply.github.com>
2017-09-12 20:37:28 -0300

    Add files via upload

---
+++ pages/index/index.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-index',
+  templateUrl: 'index.html'
+})
+export class IndexPage {
+
+  constructor(public navCtrl: NavController) {
+  }
+  
+}
